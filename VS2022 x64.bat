ada2e9904c2570de0cd9e2cba9b9bb17bb8c9416
Darkness <amir008hosein@gmail.com>
2023-01-26 13:30:13 +0330

    Updated the build script to automatically fetch the submodules and build the Release config

--- VS2022 x64.bat
+++ VS2022 x64.bat
@@ -1,6 +1,8 @@
+git submodule init
+git submodule update
 mkdir "VS2022 x64"
 cd "VS2022 x64"
 cmake .. -G "Visual Studio 17 2022" -A x64
-::cmake --build . --config Release
+cmake --build . --config Release
 cd ..
 pause
--- VS2022 x86.bat
+++ VS2022 x86.bat
@@ -1,6 +1,8 @@
+git submodule init
+git submodule update
 mkdir "VS2022 x86"
 cd "VS2022 x86"
 cmake .. -G "Visual Studio 17 2022" -A Win32
-::cmake --build . --config Release
+cmake --build . --config Release
 cd ..
 pause
